78c4f2efd7b86540d725168c98ea5e487a5d63f0
Alejandra Pinto <konshuelo@hotmail.com>
2025-09-22 14:36:14 -0300

    Create Test_9.sql

---
+++ Tests/Test_9.sql
@@ -0,0 +1,35 @@
+--Validación de datos mediante disparadores en la base de datos de recurso humano.
+--Para optimizar la administración de recursos en un departamento específico, se busca implementar triggers en la base de datos. Esta tarea 
+--implica la creación de triggers que respondan a eventos específicos, como la actualización de datos o la inserción de registros. Estos 
+--triggers buscan automatizar tareas recurrentes, mejorar la integridad de los datos y garantizar un rendimiento eficiente en la gestión de 
+--recursos del departamento.
+
+--Pregunta 1: ¿Cuál de las siguientes afirmaciones es cierta con relación a los disparadores o triggers? Seleccione una:
+--a.Los usuarios pueden ejecutar explícitamente los disparadores.
+--b.Los disparadores se pueden definir a nivel de base de datos.
+--c.Los disparadores se pueden definir en tablas, pero no en vistas.
+--d.Los disparadores sólo se ejecutan con sentencias DML.
+
+  
+--Pregunta 2: ¿Qué estrategia seguiría para mejorar la seguridad del servidor Oracle usando disparadores o triggers? Seleccione una:
+--a.Los disparadores permiten el acceso a una tabla según los valores de datos.
+--b.Los disparadores implantan las reglas complejas de integridad.
+--c.Los disparadores copian tablas de forma síncrona en las réplicas.
+--d.Los disparadores calculan los valores de datos derivados de forma automática.
+
+  
+Pregunta 3
+Sin responder aún
+Puntúa como 1
+Marcar pregunta
+Enunciado de la pregunta
+¿Cuál de los siguientes tipos de temporización de un disparador ejecuta el cuerpo del disparador en lugar la sentencia disparadora?
+Seleccione una:
+a.
+BEFORE.
+b.
+AFTER.
+c.
+INSTEAD OF.
+d.
+FOR EACH ROW. 
