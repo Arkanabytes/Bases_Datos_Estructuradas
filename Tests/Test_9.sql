40358f0d878fec5566bba7b84b9141f051b1d452
Alejandra Pinto <konshuelo@hotmail.com>
2025-09-22 14:45:06 -0300

    Update Test_9.sql

--- Tests/Test_9.sql
+++ Tests/Test_9.sql
@@ -18,18 +18,51 @@
 --d.Los disparadores calculan los valores de datos derivados de forma automática.
 
   
-Pregunta 3
-Sin responder aún
-Puntúa como 1
-Marcar pregunta
-Enunciado de la pregunta
-¿Cuál de los siguientes tipos de temporización de un disparador ejecuta el cuerpo del disparador en lugar la sentencia disparadora?
-Seleccione una:
-a.
-BEFORE.
-b.
-AFTER.
-c.
-INSTEAD OF.
-d.
-FOR EACH ROW. 
+--Pregunta 3: ¿Cuál de los siguientes tipos de temporización de un disparador ejecuta el cuerpo del disparador en lugar la sentencia 
+--disparadora?
+
+--a.BEFORE.
+--b.AFTER.
+--c.INSTEAD OF.
+--d.FOR EACH ROW. 
+
+--Pregunta 4:Usted crea un disparador llamado RESTRICT_SALARY con la sentencia mostrada a continuación: Tomando en cuenta que el empleado con el ID 121 
+--existe, ¿cuál es el resultado de la ejecución la sentencia?
+
+CREATE OR REPLACE TRIGGER restrict_salary
+BEFORE INSERT OR UPDATE OF salary ON employees
+FOR EACH ROW
+BEGIN
+IF :NEW.salary > 15000 THEN
+RAISE_APPLICATION_ERROR (-20202,'Employee salary error.');
+END IF;
+END;
+
+--Sentencia ejecutada por BOB:
+UPDATE employees SET salary = 15550
+WHERE employee_id = 121;
+
+--a.La sentencia se ejecuta correctamente y una fila se actualiza.
+--b.La sentencia se ejecuta correctamente, pero cero filas se actualizan.
+--c.La sentencia falla con el error ORA-00933.
+--d.La sentencia falla con el error ORA-20202.
+
+
+--Pregunta 5: Usted crea un disparador llamado AUDIT_EMP_VALUES con la siguiente sentencia SQL. ¿Cuál de las siguientes afirmaciones es 
+--cierta con relación a la creación del disparador?
+
+CREATE OR REPLACE TRIGGER audit_emp_values
+AFTER DELETE OR INSERT OR UPDATE ON employees
+BEGIN
+INSERT INTO audit_emps VALUES (USER, SYSDATE, :OLD.employee_id,
+:OLD.last_name, :NEW.last_name, :OLD.job_id,
+:NEW.job_id, :OLD.salary, :NEW.salary);
+END;
+
+--a.El disparador compila exitosamente.
+--b.No es necesario que la tabla AUDIT_EMPS exista antes de crear el disparador.
+--c.La compilación falla porque NEW y OLD no han sido declarados.
+--d.La compilación falla porque el disparador es de nivel de sentencia.
+
+
+
